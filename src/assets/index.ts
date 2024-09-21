d43ed5dd5d3f739eded84d4d4e1ced0282a2b506
Vishwa Priya I <vishwapriyai248@gmail.com>
2024-09-21 19:11:09 +0530

    Optimize images for better performance

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -17,23 +17,23 @@ import threejs from "./tech/threejs.svg";
 
 import digitalmagic from "./company/digitalmagic.png";
 
-import banshee_avatar_3 from "./banshee_avatar_3.jpeg";
-import game_character_1 from "./game_character_1.jpeg";
-import girl_2 from "./girl_2.jpeg";
-import head_1 from "./head_1.jpeg";
-import head_spin from "./head_spin.jpeg";
+import banshee_avatar_3 from "./banshee_avatar_3.png";
+import game_character_1 from "./game_character_1.png";
+import girl_2 from "./girl_2.png";
+import head_1 from "./head_1.png";
+import head_spin from "./head_spin.png";
 import house_1 from "./house_1.jpeg";
-import japan_master_1 from "./japan_master_1.jpeg";
-import man_human_fusion_2 from "./man_human_fusion_2.jpeg";
-import mosquito_robot_1 from "./mosquito_robot_1.jpeg";
+import japan_master_1 from "./japan_master_1.png";
+import man_human_fusion_2 from "./man_human_fusion_2.png";
+import mosquito_robot_1 from "./mosquito_robot_1.png";
 import resume from "./resume.pdf";
-import robot from "./robot.jpeg";
+import robot from "./robot.png";
 import phone from "./phone.jpeg";
-import saber_toothed_tiger from "./saber_toothed_tiger.jpeg";
-import sci_fi_gun_1 from "./sci_fi_gun_1.jpeg";
+import saber_toothed_tiger from "./saber_toothed_tiger.png";
+import sci_fi_gun_1 from "./sci_fi_gun_1.png";
 import snake from "./snake.jpeg";
 import vinayagar from "./vinayagar.jpeg"
-import water_monster_2 from "./water_monster_2.jpeg";
+import water_monster_2 from "./water_monster_2.png";
 
 export {
   digitalmagic,
